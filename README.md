7835d0f03bb6f9dce1ea239784fedfce2d0bc750
lxx <lxx@lnmt.(none)>
2017-10-06 14:41:04 +0800

    Flask Web开发，基于python的应用开发实践 2c

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# flaskytest
-Flask Web Developed instances test
+Flasky
+======
+
+This repository contains the source code examples for my O'Reilly book [Flask Web Development](http://www.flaskbook.com).
+
+The commits and tags in this repository were carefully created to match the sequence in which concepts are presented in the book. Please read the section titled "How to Work with the Example Code" in the books preface for instructions.
+
+
+Lxx copy the repository for learn, Prey for my family!!
---
+++ hello.py
@@ -0,0 +1,38 @@
+#!/usr/bin/evn python
+
+from flask import Flask, render_template
+from flask import request 
+from flask import redirect
+from flask import abort
+# from flask_script import Manager
+
+app = Flask(__name__)
+
+# manager = Manager(app)
+
+
+@app.route('/')
+def index():
+	# return '<h1>Hello world!</h1>'
+	return render_template('index.html')
+
+@app.route('/browser')
+def browser():
+	user_agent = request.headers.get('User-Agent')
+	return '<p>Your Browser is %s </p>'  % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+	# return '<h1>Hello ,%s!</h1>'  % name
+	return render_template('user.html', name=name)
+
+@app.route('/redirect')
+def baiduredirect():
+	return redirect('http://www.baidu.com')
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+#	manager.run()
+
